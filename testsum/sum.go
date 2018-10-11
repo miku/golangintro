6aca5d2ccc1ab3c1ddab413273c7a46f197d16a1
Martin Czygan <martin.czygan@gmail.com>
2018-10-12 00:44:54 +0200

    add testing

---
+++ testsum/sum.go
@@ -0,0 +1,5 @@
+package testsum
+
+func Sum(x int, y int) int {
+	return x + y
+}
---
+++ testsum/sum_test.go
@@ -0,0 +1,34 @@
+// package main
+
+// import "testing"
+
+// func TestSum(t *testing.T) {
+// 	total := Sum(5, 5)
+// 	if total != 10 {
+// 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+// 	}
+// }
+
+package testsum
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	tables := []struct {
+		x int
+		y int
+		n int
+	}{
+		{1, 1, 2},
+		{1, 2, 3},
+		{2, 2, 4},
+		{5, 2, 7},
+	}
+
+	for _, table := range tables {
+		total := Sum(table.x, table.y)
+		if total != table.n {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
+		}
+	}
+}
