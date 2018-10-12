22fe7aae1bd1244da75e8cce1b183bd989a5efd7
Martin Czygan <martin.czygan@gmail.com>
2018-10-12 06:03:29 +0200

    furl1, 2

---
+++ s/furl1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://devopenspace.de")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ s/furl2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("small.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	br := bufio.NewReader(f)
+
+	for {
+		line, err := br.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		link := strings.TrimSpace(line)
+		resp, err := http.Get(link)
+		if err != nil {
+			log.Printf("failed %s: %s", link, err)
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode < 400 {
+			log.Printf("ok[%d]: %s", resp.StatusCode, link)
+		} else {
+			log.Printf("failed[%d]: %s", resp.StatusCode, link)
+		}
+	}
+}
